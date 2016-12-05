7c3ae365e325115913eb75ecd6f4e4b996f91a9a
Richard King <richrdkng@gmail.com>
2016-12-05 22:28:36 +0000

    Release v0.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='pytools-sshcli',
 
-    version='0.1.0',
+    version='0.1.1',
 
     description='A helper module wrapped around common CLI commands through SSH',
     long_description=long_description,
